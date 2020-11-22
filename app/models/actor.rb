71a7ce9a8de1b5e4a91ef2fbb2fb18bc26bf6680
Erick Castille <ecastille924@gmail.com>
2020-11-22 19:55:24 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,9 @@ class Actor < ActiveRecord::Base
   end 
   
   def list_roles
-    self.characters.collect do |t|
-      t.characters 
+    self.characters.collect do |c|
+      c.name 
+      c.show_name 
     end
   end
   
