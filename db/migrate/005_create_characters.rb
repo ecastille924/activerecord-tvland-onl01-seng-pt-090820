98e73d351adb203e286dca000c4aed1dd496bec0
Erick Castille <ecastille924@gmail.com>
2020-11-22 19:35:22 +0000

    Automatically backed up by Learn

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,8 @@
+
+#- Write a migration for the characters table. A character should have a `name`,
+#  `actor_id`, and a `show_id`––a character will belong to a show (the show
+#  migration is already provided) and an actor, and we'll keep track of this
+#  relationship with these database table columns.
+#- Write a migration that adds the column `catchphrase` to your character model.
+
+def change 
