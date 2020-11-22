69fc5b797f244cf4027522a2d5713306609ca1f5
Erick Castille <ecastille924@gmail.com>
2020-11-22 19:31:20 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,7 +1,8 @@
 #- Write a migration for the actors table. An actor should have a `first_name`
-  and a `last_name`.
-- Write a migration for the characters table. A character should have a `name`,
-  `actor_id`, and a `show_id`––a character will belong to a show (the show
-  migration is already provided) and an actor, and we'll keep track of this
-  relationship with these database table columns.
-- Write a migration that adds the column `catchphrase` to your character model.
+#  and a `last_name`.
+#- Write a migration for the characters table. A character should have a `name`,
+#  `actor_id`, and a `show_id`––a character will belong to a show (the show
+#  migration is already provided) and an actor, and we'll keep track of this
+#  relationship with these database table columns.
+#- Write a migration that adds the column `catchphrase` to your character model.
+
